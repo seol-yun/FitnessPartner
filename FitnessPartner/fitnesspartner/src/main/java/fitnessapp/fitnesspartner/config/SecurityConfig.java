981861030c505037946bfc634cd88f8608222cab
yunHwan Seol <dyj07132@gmail.com>
2024-08-13 14:41:34 +0900

    비번 해싱

---
+++ FitnessPartner/fitnesspartner/src/main/java/fitnessapp/fitnesspartner/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package fitnessapp.fitnesspartner.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+        http
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                .requestMatchers("**").permitAll()
+                                .requestMatchers("api/**").permitAll()
+                                .requestMatchers("chat/**").permitAll()
+                                .requestMatchers("/chat-websocket/**").permitAll()
+                                .requestMatchers("reviews/**").permitAll()
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .requestMatchers("/error").permitAll()
+                                .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ FitnessPartner/fitnesspartner/src/main/java/fitnessapp/fitnesspartner/service/FirewallService.java
@@ -0,0 +1,21 @@
+package fitnessapp.fitnesspartner.service;
+
+import fitnessapp.fitnesspartner.controller.SfcPacket;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class FirewallService {
+    private final List<String> allowedIPs = List.of("192.168.1.1", "127.0.0.1"); // 예시 IP 목록
+
+    public SfcPacket filter(SfcPacket packet) {
+        String clientIP = packet.getData().get("clientIP"); // 클라이언트 IP를 패킷에서 가져옵니다.
+
+        if (allowedIPs.contains(clientIP)) {
+            packet.setAuthenticated(true); // 허용된 IP라면 인증된 것으로 표시합니다.
+        } else {
+            packet.setAuthenticated(false); // 허용되지 않은 IP라면 인증되지 않은 것으로 표시합니다.
+        }
+        return packet;
+    }
+}
